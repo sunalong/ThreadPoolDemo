e80d5aab5dde00a408d4baaccaed99e0b2cd7342
sunalong <sunalong@ztgame.com>
2018-05-02 11:49:41 +0800

    修改线程callable以便能正确获取返回值

--- src/com/itcode/javaadv/callable/CallableAndFuture.java
+++ src/com/itcode/javaadv/callable/CallableAndFuture.java
@@ -29,7 +29,7 @@ class CallableTask implements Callable {
         if (this.flag == 1) {
             try {
                 while (true) {
-                    System.out.println(name + ": looping...");
+                    System.out.println(name + ": looping..."+Thread.currentThread().getName());
                     Thread.sleep(2000);
                 }
             } catch (InterruptedException e) {
@@ -37,7 +37,8 @@ class CallableTask implements Callable {
             }
             return "false";
         } else {
-            throw new Exception(name + ":Bad flag value!");
+//            throw new Exception(name + ":Bad flag value!");
+            return new Exception(name + ":Bad flag value!").toString();
         }
     }
 }
@@ -45,26 +46,28 @@ class CallableTask implements Callable {
 public class CallableAndFuture {
 
     public static void main(String[] args) {
-
-        // 定义3个Callable类型的任务
-        CallableTask task1 = new CallableTask("FirstTask", 0);
-        CallableTask task2 = new CallableTask("SecondTask", 1);
-        CallableTask task3 = new CallableTask("ThirdTask", 2);
-
-        FutureTask futureTask1 = new FutureTask(task1);
-        // 创建一个执行任务的线程池
-        ExecutorService es = Executors.newFixedThreadPool(3);
         try {
+            // 定义3个Callable类型的任务
+            CallableTask task1 = new CallableTask("FirstTask", 0);
+            CallableTask task2 = new CallableTask("SecondTask", 1);
+            CallableTask task3 = new CallableTask("ThirdTask", 2);
+
+            // 创建一个执行任务的线程池
+            ExecutorService es = Executors.newFixedThreadPool(3);
+
+
             Future future1 = es.submit(task1);
             System.out.println(task1.getName() + ":的执行结果Future调用get方法:" + future1.get());
 
+            FutureTask futureTask1 = new FutureTask(task1);
             es.submit(futureTask1);
-            System.out.println(task1.getName() + ":的执行结果Future调用get方法:" + futureTask1.get());
-          
+            System.out.println(task1.getName() + ":的执行结果FutureTask调用get方法:" + futureTask1.get());
+
             //FutureTask的第二种使用方式
-            FutureTask<String> futureTask = new FutureTask<>(task1);
-            Thread thread = new Thread(futureTask);
+            FutureTask<String> futureTask2 = new FutureTask<>(task1);
+            Thread thread = new Thread(futureTask2);
             thread.start();
+            System.out.println(task1.getName() + ":的执行结果Future调用get方法:" + futureTask2.get());
 
             Future future2 = es.submit(task2);
             Thread.sleep(5000);
@@ -72,11 +75,11 @@ public class CallableAndFuture {
 
             Future future3 = es.submit(task3);
             System.out.println(task3.getName() + "：的执行结果Future调用get方法：" + future3.get());
+            // 停止任务执行服务
+            es.shutdownNow();
         } catch (Exception e) {
             System.out.println(e.toString());
         }
-        // 停止任务执行服务
-        es.shutdownNow();
 
     }
 }
